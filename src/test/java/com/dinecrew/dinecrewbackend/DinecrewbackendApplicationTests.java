633b9780c276afebfb0603b21fdfd8da783f1032
mjmp0027 <mjmp0027@red.ujaen.es>
2024-07-04 12:38:57 +0200

    Inicio del backend

---
+++ src/test/java/com/dinecrew/dinecrewbackend/DinecrewbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dinecrew.dinecrewbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DinecrewbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
