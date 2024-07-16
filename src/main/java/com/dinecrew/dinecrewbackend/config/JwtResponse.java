07bb101abfda353f8ea6f8354695736e732fba49
mjmp0027 <mjmp0027@red.ujaen.es>
2024-07-16 18:36:51 +0200

    warnings eliminados

--- src/main/java/com/dinecrew/dinecrewbackend/config/JwtResponse.java
+++ src/main/java/com/dinecrew/dinecrewbackend/config/JwtResponse.java
@@ -1,14 +1,13 @@
 package com.dinecrew.dinecrewbackend.config;
 
+import lombok.Getter;
+
+@Getter
 public class JwtResponse {
     private String token;
 
     public JwtResponse(String token) {
         this.token = token;
-    }
-
-    public String getToken() {
-        return token;
     }
 
     public void setToken(String token) {
