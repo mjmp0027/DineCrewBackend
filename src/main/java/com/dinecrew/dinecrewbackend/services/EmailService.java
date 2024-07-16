4635c383dbb0447b35362fa8183f04fb6ea5a533
mjmp0027 <mjmp0027@red.ujaen.es>
2024-07-16 18:35:17 +0200

    Implementa el servicio de email para poder enviar correos para restablecer la contraseña

---
+++ src/main/java/com/dinecrew/dinecrewbackend/services/EmailService.java
@@ -0,0 +1,28 @@
+package com.dinecrew.dinecrewbackend.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void sendEmail(String to, String subject, String content) throws MessagingException {
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+
+        helper.setFrom("manueljesusmoralesperez@gmail.com");
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setText(content, true); // Configura el contenido como HTML
+
+        mailSender.send(message);
+    }
+}
